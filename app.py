b2befef33c1838d4722cf0a4d2577d6465d15583
AbdulBhashith <64657574+AbdulBhashith@users.noreply.github.com>
2024-03-05 17:37:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     user_agent = request.headers.get('User-Agent')
-    return f"Welcome to 2022! User Agent: {user_agent}"
+    return f"Welcome to 2024! User Agent: {user_agent}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
